715edfeac25ab58b1918d039c3c0511952da7077
Makoto Ito <millionlives@gmail.com>
2018-05-17 15:22:29 +0900

    fix

--- lib/ruboty/pokedex/actions/pokedex.rb
+++ lib/ruboty/pokedex/actions/pokedex.rb
@@ -2,30 +2,32 @@ require 'faraday'
 require 'faraday_middleware'
 
 module Ruboty
-    module Actions
-        class Pokedex < Ruboty::Actions::Base
-            POKEDEX_API = 'http://pokeapi.co/api/v2/'
+    module Pokedex
+        module Actions
+            class Pokedex < Ruboty::Actions::Base
+                POKEDEX_API = 'http://pokeapi.co/api/v2/'
 
-            def call
-                keyword = message[:number]
-                resp = connection.get(POKEDEX_API + "pokemon-species/#{keyword}")
+                def call
+                    keyword = message[:number]
+                    resp = connection.get(POKEDEX_API + "pokemon-species/#{keyword}")
 
-                id = resp.body['id']
-                name = resp.body['names'].select do |x|
-                    x.language.name == "ja"
+                    id = resp.body['id']
+                    name = resp.body['names'].select do |x|
+                        x.language.name == "ja"
+                    end
+                    desc = resp.body['flavor_text_entries'].select do |x|
+                        x.language.name = "ja"
+                    end
+                    res = "No.#{id} #{name[0]}\n#{desc[0]}"
+
+                    message.reply(res)
                 end
-                desc = resp.body['flavor_text_entries'].select do |x|
-                    x.language.name = "ja"
-                end
-                res = "No.#{id} #{name[0]}\n#{desc[0]}"
 
-                message.reply(res)
-            end
-
-            def connection
-                Faraday.new do |connection|
-                    connection.adapter :net_http
-                    connection.response :json
+                def connection
+                    Faraday.new do |connection|
+                        connection.adapter :net_http
+                        connection.response :json
+                    end
                 end
             end
         end
