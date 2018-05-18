8923d3198195bded4dc3cd7bed294a25a14d08fb
Makoto Ito <millionlives@gmail.com>
2018-05-18 15:28:47 +0900

    fix regex

--- lib/ruboty/handlers/pokedex.rb
+++ lib/ruboty/handlers/pokedex.rb
@@ -5,7 +5,7 @@ module Ruboty
     module Handlers
         class Pokedex < Base
             on(/pokedex\s+(?<number>\d+).*/, name: 'num', description: 'search from number', all: true)
-            on(/pokedex\s+(?<name>\p{katakana}+).*/, name: 'name', description: 'search from name', all: true)
+            on(/pokedex\s+(?<name>\p{katakana}+)/, name: 'name', description: 'search from name', all: true)
 
             def num(msg)
                 Ruboty::Pokedex::Actions::Pokedex.new(msg).call
