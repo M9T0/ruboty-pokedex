16b93b42a09afbf78eda29ff4d676b0984934c7d
Makoto Ito <millionlives@gmail.com>
2018-05-18 13:13:35 +0900

    add api cache

--- lib/ruboty/pokedex/actions/pokedex.rb
+++ lib/ruboty/pokedex/actions/pokedex.rb
@@ -6,18 +6,17 @@ module Ruboty
     module Pokedex
         module Actions
             class Pokedex < Ruboty::Actions::Base
-                POKEDEX_API = 'http://pokeapi.co/api/v2/'
+                NAMESPACE = 'POKEDEX'
+                POKEDEX_API = 'https://pokeapi.co/api/v2/'
+
+                def brain
+                    robot.brain.data[NAMESPACE] ||= {}
+                end
 
                 def call
                     keyword = message[:number]
 
-                    uri = URI("https://pokeapi.co/api/v2/pokemon-species/#{keyword}/")
-                    req = Net::HTTP::Get.new(uri)
-                    resp = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') { |http|
-                        http.request(req)
-                    }
-                    json = JSON.parse(resp.body)
-
+                    json = get(POKEDEX_API + "pokemon-species/#{keyword}/")
                     id = json['id']
                     name = json['names'].select do |x|
                         x['language']['name'] == "ja"
@@ -25,9 +24,27 @@ module Ruboty
                     desc = json['flavor_text_entries'].select do |x|
                         x['language']['name'] == "ja"
                     end
-                    res = "No.#{id} #{name[0]['name']}\n#{desc[0]['flavor_text']}"
+                    res = "No.#{id} #{name[0]['name']}\n"
+                    desc.each do |item|
+                        res += "#{item['flavor_text']}\n～#{item['version']['name']}～\n"
+                    end
 
                     message.reply(res)
+                end
+
+                def get(url)
+                    if brain.has_key?(url)
+                        brain[url]
+                    else
+                        uri = URI(url)
+                        req = Net::HTTP::Get.new(uri)
+                        resp = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) { |http|
+                            http.request(req)
+                        }
+                        json = JSON.parse(resp.body)
+                        brain[url] = json
+                        json
+                    end
                 end
             end
         end
